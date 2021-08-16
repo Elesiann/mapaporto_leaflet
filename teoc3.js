5fae83b37a1241263de9fe9cd03d1ff4d723d600
Giovani <giovanimcorrea18@gmail.com>
2021-08-16 20:16:57 -0300

    markers

---
+++ teoc3.js
@@ -0,0 +1,40 @@
+let teoc3JSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "stroke": "#555555",
+          "stroke-width": 2,
+          "stroke-opacity": 1,
+          "fill": "#555555",
+          "fill-opacity": 0.5,
+          "id": 6,
+          "name": "TEOC 3",
+        },
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [
+                -48.65830779075622,
+                -28.231677515513123
+              ],
+              [
+                -48.65438103675842,
+                -28.2304959395007
+              ],
+              [
+                -48.65478873252869,
+                -28.229758629440294
+              ],
+              [
+                -48.65830779075622,
+                -28.231677515513123
+              ]
+            ]
+          ]
+        }
+      }
+    ]
+  }
